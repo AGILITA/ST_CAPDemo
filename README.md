1ce092dbe7ad30fbdb9b6087a2e1bbb549a84168
AgilitaDaniel <75628485+AgilitaDaniel@users.noreply.github.com>
2021-09-16 08:48:30 +0200

    linting

--- README.md
+++ README.md
@@ -31,3 +31,5 @@ Weitere Information und Dokumentationen.
 - [Fiori Elements Dokumentation](https://ui5.sap.com/#/topic/03265b0408e2432c9571d6b3feb6b1fd)
 - [Cloud SDK](https://sap.github.io/cloud-sdk/)
 - [AGILITA BTP](https://www.agilita.ch/produkte/sap-business-technology-platform/)
+- [SAP Community](https://community.sap.com/)
+
--- srv/travel-service.js
+++ srv/travel-service.js
@@ -1,17 +1,12 @@
 const cds = require('@sap/cds');
 
 class travel extends cds.ApplicationService {
+    // eslint-disable-next-line no-unused-vars
     async init(srv) {
-
         this.on('getRandomDestination', async (req) => { 
             const {name} = req.data;
             return 'Zurich Hallo ' + name
         });
-
-        // this.after('READ', 'Destinations', async (dests) =>{
-        //     return dest.map()
-        // } )
-
         return super.init();
     }
 }
